f786c8fde08567b03b677a20931270aa7ca625a8
CharlieRojas <crc_185@yahoo.com>
2022-03-17 11:23:30 -0500

    commit tree eb688b80f8230ef9fcc00fb95bb44e4cc28a38a7

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+function writeCards(names, event){
+const cards = [];
+for(let i= 0; i < names.length;i++){
+    cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`); 
+};
+return cards;
+};
+function countDown(int){
+for(let i= int; i>=0;i--){
+    console.log(i)
+}
+}
